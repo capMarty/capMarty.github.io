cfa497324d225cbbb926d71a577ba14b3a0b175e
Sergey Shukov <shukovsregey1995@gmail.com>
2019-06-06 10:18:50 +0300

    add eventlistener on inputs

--- js/checkForm.js
+++ js/checkForm.js
@@ -1,14 +1,39 @@
-let array = [];
-let totalarr = [];
-let form = document.querySelectorAll('.box');
+let form = document.forms[0];
+let arrayElem = [];
+let text;
 
 
-form.forEach( elem => {
-	array.push(elem.children);
+for (let i = 0; i<form.elements.length; i++){
+	if(form.elements[i].type !== "submit") 
+		arrayElem[i] = form[i]
+}
+
+arrayElem.forEach( elem => { 
+	elem.onfocus = function () {
+		text = elem.getAttribute("placeholder");
+		elem.setAttribute("placeholder", "");
+		elem.onblur = function () {
+			elem.setAttribute("placeholder", text)
+		}
+	}
 })
 
-for (let i = 0; i < array.length; i++){
-	for( let j = 0; j < array[i].length; j++){
-		totalarr.push(array[i][j]);
-	}
-}
+// form.onsubmit = validator;
+
+// let ruler = {
+// 	requrid: function(elem){
+// 		if (elem.value != '')
+// 			return true;
+// 		return false;	 	
+// 	},
+// 	email: function (elem) {
+// 		let regExp = /^[\w.]+@[\w.]+\.\w.]+$/;
+// 		return regExp.test(elem.value)
+// 	}
+// }
+
+// function validator () {
+// 		let error = [];
+
+// 	}	
+
